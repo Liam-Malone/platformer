ed7651ef383c8a000dc512b98d51cf8c4a5ba940
MediocreDev <liammaloneirlusa@gmail.com>
2023-12-15 05:49:42 +0000

    better collision

--- src/main.zig
+++ src/main.zig
@@ -151,13 +151,12 @@ const FPS = 60;
 const FRAME_DELAY = 1000 / FPS;
 const BACKGROUND_COLOR = Color.dark_gray;
 const SCREEN_WIDTH = 1200;
-const SCREEN_HEIGHT = 800;
+const SCREEN_HEIGHT = 900;
 const TILE_WIDTH = 32;
 const TILE_HEIGHT = 32;
 // END CONSTANTS
 
 //BEGIN FUNCTIONS
-
 fn collide(rect_a: *const c.SDL_Rect, rect_b: *const c.SDL_Rect) bool {
     return c.SDL_HasIntersection(rect_a, rect_b) != 0;
 }
@@ -167,8 +166,8 @@ fn direct_cam_toward_player(player: *Player, cam: *Camera) void {
     const ymid = @divFloor(cam.h, 2);
     const dx = (player.x + player.w) - (cam.x + xmid);
     const dy = (player.y + player.h) - (cam.y + ymid);
-    if (cam.x + dx > 0) cam.x += dx;
-    if (cam.y + dy > 0) cam.y += dy;
+    if (cam.x + dx > 0 and cam.x + cam.w + cam.dx < SCREEN_WIDTH) cam.x += dx;
+    if (cam.y + dy > 0 and cam.y + cam.h + cam.dy < SCREEN_HEIGHT) cam.y += dy;
 }
 fn place_at_pos(x: u32, y: u32, tilemap: *map.Tilemap, id: u8) void {
     tilemap.edit_tile(id, x / (TILE_WIDTH), y / (TILE_HEIGHT));
@@ -196,91 +195,6 @@ fn set_fullscreen(window: *c.SDL_Window, fullscreen_type: u32) void {
 fn set_render_color(renderer: *c.SDL_Renderer, color: Color) void {
     const col = Color.make_sdl_color(color);
     _ = c.SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
-}
-
-//*****************//
-//  *** TODO ***   //
-//                 //
-//    FIX THIS     //
-//    COS I AM     //
-//    BRAINDEAD    //
-//*****************//
-fn vcollide_player_with_tiles(p: *Player, tm: *map.Tilemap) bool {
-    if (p.y < 0 or p.y > SCREEN_HEIGHT) return false;
-    if (p.dy == 0) {
-        const x_idx: usize = @intCast(@divFloor(p.x, TILE_WIDTH));
-        const y_idx: usize = @intCast(@divFloor(p.y + p.h, TILE_HEIGHT));
-        if (p.y + p.h >= tm.map[y_idx][x_idx].y and tm.map[y_idx][x_idx].collides) {
-            return true;
-        } else if (p.y + p.h >= tm.map[y_idx][x_idx + 1].x and tm.map[y_idx][x_idx + 1].collides) {
-            return true;
-        }
-
-        return false;
-    }
-    switch (p.dy > 0) {
-        true => {
-            const x_idx: usize = @intCast(@divFloor(p.x, TILE_WIDTH));
-            const y_idx: usize = @intCast(@divFloor(p.y + p.h + p.dy, TILE_HEIGHT));
-            if (p.y + p.h + p.dy >= tm.map[y_idx][x_idx].y and tm.map[y_idx][x_idx].collides) {
-                if (p.dy > 0) p.dy = 0;
-                return true;
-            } else if (p.y + p.h >= tm.map[y_idx][x_idx + 1].x and tm.map[y_idx][x_idx + 1].collides) {
-                return true;
-            }
-
-            return false;
-        },
-        false => {
-            const x_idx: usize = @intCast(@divFloor(p.x, TILE_WIDTH));
-            const y_idx: usize = @intCast(@divFloor(if (p.y + p.dy > 0) p.y + p.dy else p.y, TILE_HEIGHT));
-            if (p.y + p.dy <= tm.map[y_idx][x_idx].y + tm.map[y_idx][x_idx].h and tm.map[y_idx][x_idx].collides) {
-                if (p.dy < 0) p.dy = 0;
-                return true;
-            } else if (p.y + p.dy <= tm.map[y_idx][x_idx + 1].y + tm.map[y_idx][x_idx + 1].h and tm.map[y_idx][x_idx + 1].collides) {
-                return true;
-            }
-
-            return false;
-        },
-    }
-}
-fn hcollide_player_with_tiles(p: *Player, tm: *map.Tilemap) bool {
-    if (p.x < 0 or p.x > SCREEN_WIDTH) return false;
-    if (p.dx == 0) {
-        const x_idx: usize = @intCast(@divFloor(p.x, TILE_WIDTH));
-        const y_idx: usize = @intCast(@divFloor(p.y + p.h, TILE_HEIGHT));
-        if (p.x + p.w >= tm.map[y_idx - 1][x_idx].x and tm.map[y_idx - 1][x_idx].collides) {
-            return true;
-        } else if (p.x + p.w >= tm.map[y_idx - 1][x_idx + 1].x and tm.map[y_idx - 1][x_idx + 1].collides) {
-            return true;
-        }
-        return false;
-    }
-    switch (p.dx > 0) {
-        true => {
-            const x_idx: usize = @intCast(@divFloor(p.x + p.w + p.dx, TILE_WIDTH));
-            const y_idx: usize = @intCast(@divFloor(p.y + p.h, TILE_HEIGHT));
-            if (p.x + p.w + p.dx >= tm.map[y_idx - 1][x_idx].x and tm.map[y_idx - 1][x_idx].collides) {
-                if (p.dx > 0) p.dx = 0;
-                return true;
-            } else if (p.x + p.w >= tm.map[y_idx - 1][x_idx + 1].x and tm.map[y_idx - 1][x_idx + 1].collides) {
-                return true;
-            }
-            return false;
-        },
-        false => {
-            const x_idx: usize = @intCast(@divFloor(if (p.x + p.dx > 0) p.x + p.dx else p.x, TILE_HEIGHT));
-            const y_idx: usize = @intCast(@divFloor(p.y + p.h, TILE_HEIGHT));
-            if (p.x + p.dx <= tm.map[y_idx - 1][x_idx].x + tm.map[y_idx - 1][x_idx].w and tm.map[y_idx - 1][x_idx].collides) {
-                if (p.dx < 0) p.dx = 0;
-                return true;
-            } else if (p.x + p.dx <= tm.map[y_idx - 1][x_idx + 1].x + tm.map[y_idx - 1][x_idx + 1].w and tm.map[y_idx - 1][x_idx + 1].collides) {
-                return true;
-            }
-            return false;
-        },
-    }
 }
 // END FUNCTIONS
 
@@ -332,7 +246,7 @@ pub fn main() !void {
         SCREEN_HEIGHT,
         TILE_WIDTH,
         TILE_HEIGHT,
-        "assets/maps/simple_map.map",
+        "assets/maps/new_map.map",
     );
 
     const floor = c.IMG_LoadTexture(renderer, "assets/textures/floor.png");
@@ -346,7 +260,7 @@ pub fn main() !void {
     var frame_time: u32 = undefined;
 
     var map_tex_used: ?*c.SDL_Texture = null;
-    var map_edit_enabled: bool = false;
+    var map_edit_enabled: bool = true;
 
     var left_mouse_is_down: bool = false;
     var tile_id_selected: u8 = 0;
@@ -368,8 +282,8 @@ pub fn main() !void {
                         quit = true;
                     },
                     'm' => music.?.toggle_pause(),
-                    'a' => player.dx = -2,
-                    'd' => player.dx = 2,
+                    'a' => player.dx = -6,
+                    'd' => player.dx = 6,
                     's' => {
                         if (map_edit_enabled) {
                             if (event.key.keysym.mod & c.KMOD_CTRL != 0) {
@@ -420,6 +334,59 @@ pub fn main() !void {
             }
         }
 
+        // do stuff
+        for (tmap.map) |row| {
+            for (row) |tile| {
+                switch (tile.collides) {
+                    true => {
+                        if (collide(&c.SDL_Rect{
+                            .x = player.x,
+                            .y = player.y + player.dy,
+                            .w = player.w,
+                            .h = player.h,
+                        }, &c.SDL_Rect{
+                            .x = tile.x,
+                            .y = tile.y,
+                            .w = tile.w,
+                            .h = tile.h,
+                        })) {
+                            player.dy = @divFloor(player.dy, 3);
+                        }
+                        if (collide(&c.SDL_Rect{
+                            .x = player.x + player.dx,
+                            .y = player.y,
+                            .w = player.w,
+                            .h = player.h,
+                        }, &c.SDL_Rect{
+                            .x = tile.x,
+                            .y = tile.y,
+                            .w = tile.w,
+                            .h = tile.h,
+                        })) {
+                            player.x -= @divFloor(player.dx, 2);
+                        }
+                    },
+                    false => {},
+                }
+            }
+        }
+
+        if (player.x + player.dx < 0 or player.x + player.w + player.dx > SCREEN_WIDTH) player.x -= @divFloor(player.dx, 2); // will need to change for larger maps
+
+        if (!map_edit_enabled) {
+            player.x += player.dx;
+            player.y += player.dy;
+            player.dy += 1;
+        }
+        //if (!vcollide_player_with_tiles(&player, &tmap)) {
+        //    if (player.dy < 6) player.dy += 1;
+        //    player.y += player.dy;
+        //}
+        //if (!hcollide_player_with_tiles(&player, &tmap)) {
+        //    player.x += player.dx;
+        //}
+
+        // no more doing, just draw
         set_render_color(renderer, Color.dark_gray);
         _ = c.SDL_RenderClear(renderer);
 
@@ -466,14 +433,6 @@ pub fn main() !void {
                     );
                 }
             }
-        }
-
-        if (!vcollide_player_with_tiles(&player, &tmap)) {
-            if (player.dy < 6) player.dy += 1;
-            player.y += player.dy;
-        }
-        if (!hcollide_player_with_tiles(&player, &tmap)) {
-            player.x += player.dx;
         }
 
         if (player_tex) |tex| {
